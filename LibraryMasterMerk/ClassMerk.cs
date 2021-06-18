a467fcace7c709be2b616c1407c0cac043e7d8e3
JasonZen421 <75821987+JasonZen421@users.noreply.github.com>
2021-06-18 17:53:41 +0700

    Add files via upload
    getMerkID

--- LibraryMasterMerk/ClassMerk.cs
+++ LibraryMasterMerk/ClassMerk.cs
@@ -109,5 +109,40 @@ namespace LibraryMasterMerk
             }
             return merkList;
         }
+
+        //
+        public static List<MasterMerk> getByMerkID(int id)
+        {
+            List<MasterMerk> merkList = new List<MasterMerk>();
+            SqlConnection connection = new SqlConnection(@"Data Source=.\SQLExpress;Initial Catalog=UAS;Integrated Security=True");
+            string selectStatement =
+                "SELECT ID,   MERK_DESC " +
+                "FROM m_merk " +
+                "where ID =" + id;
+            SqlCommand selectCommand = new SqlCommand(selectStatement, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = selectCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    MasterMerk mastermerk = new MasterMerk();
+                    mastermerk.Id = (int)reader["ID"];
+
+                    mastermerk.Merk_desc = reader["MERK_DESC"].ToString();
+                    merkList.Add(mastermerk);
+                }
+                reader.Close();
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return merkList;
+        }
     }
 }
